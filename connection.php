3f9f73fe67280328e707064f72fd88635c870bc7
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2023-04-25 21:15:33 +0200

    php crud

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername='localhost';
+$dbname='Student_db';
+$username='root';
+$password='';
+
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+if($conn ->connect_error) {
+  exit("connection success".$conn->connect_error);
+}else{
+    echo"connetion successful";
+}
+
+
+?>
